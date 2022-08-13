290d36de68919c4e9eff84776433bb51a81d5434
Weslilo <wdielsew@gmail.com>
2022-08-13 17:52:21 -0300

    up

---
+++ Aula2/Teste/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try 
+            {
+                Console.Write("Informe uma Largura: ");
+                // string largura = Console.ReadLine();
+                double largura = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Informe uma Altura: ");
+                //string altura = Console.ReadLine();
+                double altura = Convert.ToDouble(Console.ReadLine());
+            
+                Console.Write("A área equivale a: " + CalculaArea(largura, altura));
+            }
+        
+            catch (Exception erro)
+            {
+                Console.WriteLine(erro.Message);
+            }
+
+            static double CalculaArea (double largura, double altura) 
+            {
+                 return largura * altura;
+            }
+        }
+    }
+}
