cfe6d7c61d2c6935d6a970868d5689d275370d3d
Weslei Barbosa <101604299+wesleiads@users.noreply.github.com>
2022-11-25 01:23:43 -0300

    Update cadastrar-jogo.component.ts

--- FRONT_API/src/app/components/pages/jogo/cadastrar-jogo/cadastrar-jogo.component.ts
+++ FRONT_API/src/app/components/pages/jogo/cadastrar-jogo/cadastrar-jogo.component.ts
@@ -1,3 +1,4 @@
+import { ɵNoopAnimationStyleNormalizer, ɵWebAnimationsStyleNormalizer } from "@angular/animations/browser";
 import { HttpClient } from "@angular/common/http";
 import { Component, OnInit } from "@angular/core";
 import { MatSnackBar } from "@angular/material/snack-bar";
@@ -11,98 +12,55 @@ import { Selecao } from "src/app/models/selecao.model";
   styleUrls: ["./cadastrar-jogo.component.css"],
 })
 export class CadastrarJogoComponent implements OnInit {
-  id!: number;
-  selecaoA!: Selecao[];
-  selecaoB!: Selecao[];
-  criadoEm!: string;
-  placar!: Number;
-  placar1!: Number;   
+ 
+  selecoes!: Selecao[];
   mensagem!: string;
+  selecaoIdA!: number;
+  selecaoIdB!: number;
+  placar!: number;
+  placar1!: number;
 
 
-
-  constructor(private http: HttpClient, private router: Router, private route: ActivatedRoute, private _snackBar: MatSnackBar) {}
+  constructor(private http: HttpClient, private router: Router, private route: ActivatedRoute, private _snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
     this.http.get<Selecao[]>("https://localhost:5001/api/selecao/listar").subscribe({
       next: (selecoes) => {
-        this.selecaoA = selecoes;
-        
-       
+        this.selecoes = selecoes;
    
-      },
-    });
-
-    this.http.get<Selecao[]>("https://localhost:5001/api/selecao/listar").subscribe({
-      next: (selection) => {
-        this.selecaoB = selection;
-        
-       
-   
-      },
-    });
-  
-  
-
-    this.route.params.subscribe({
-      next: (params) => {
-        let { id } = params;
-        if (id !== undefined) {
-          this.http.get<Jogo>(`https://localhost:5001/api/jogo/buscar/${id}`).subscribe({
-            next: (aluguel) => {
-              this.id = id;
-              this.selecaoA;
-              this.placar;
-              this.placar1;
-              this.criadoEm;
-             
-            },
-          });
-        }
       },
     });
   }
-
-
   alterar(): void {
-      let jogo: Jogo = {
-      id: this.id,
+    let jogo: Jogo = {
       placar:this.placar,
-      criadoEm: this.criadoEm,
+      placar1: this.placar1,
       
-    };
+    }; 
     this.http.patch<Jogo>(" https://localhost:5001/api/jogo/alterar", jogo)
       .subscribe({
         next: (jogo) => {
-          this._snackBar.open("Aluguel alterado!", "Ok!", {
+          this._snackBar.open("O jogo foi alterado!", "Ok!", {
             horizontalPosition: "center",
             verticalPosition: "top",
           });
-        this.router.navigate(["pages/jogo/listar"]);
-              
-      },
-    });
+          this.router.navigate(["pages/jogo/listar"]);
+        }
+      });
   }
- 
-  cadastrar(): void {
-
-
-    let jogo: Jogo = {
-      id: this.id,
-      placar:this.placar,
-      criadoEm: this.criadoEm,
       
-    };
-
+  cadastrar(): void {
+    let jogo: Jogo = {
+      "selecaoAId": this.selecaoIdA,
+      "selecaoBId": this.selecaoIdB,
+      }
     /*Configurando a requisição para a API*/
-
-	
     this.http
       .post<Jogo>(" https://localhost:5001/api/jogo/cadastrar", jogo)
       // Executar a requisição
       .subscribe({
         next: (jogo) => {
-          this._snackBar.open("Aluguel cadastrado!", "Ok!", {
+          this._snackBar.open("O jogo foi cadastrado!", "Ok!", {
             horizontalPosition: "center",
             verticalPosition: "top",
           });
