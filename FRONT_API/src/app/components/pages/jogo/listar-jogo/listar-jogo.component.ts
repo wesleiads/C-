d82a4c8acad80e1dede004fe877bafd491800abb
Weslei Barbosa <101604299+wesleiads@users.noreply.github.com>
2022-11-25 01:25:21 -0300

    Update listar-jogo.component.ts

--- FRONT_API/src/app/components/pages/jogo/listar-jogo/listar-jogo.component.ts
+++ FRONT_API/src/app/components/pages/jogo/listar-jogo/listar-jogo.component.ts
@@ -1,3 +1,4 @@
+import { Jogo } from 'src/app/models/jogo.model';
 import { HttpClient } from "@angular/common/http";
 import { Component, OnInit } from "@angular/core";
 
@@ -7,7 +8,25 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./listar-jogo.component.css"],
 })
 export class ListarJogoComponent implements OnInit {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
+  jogos!: Jogo[];
+  id!: number;
+    
+  ngOnInit(): void {
+ 
+    //Configurando a requisição para a API
+    this.http.
+      get<Jogo[]>(
+        "https://localhost:5001/api/jogo/listar"
+      )
+      // Executar a requisição
+      .subscribe({
+        next: (jogos) => {
+          //Executamos o que for necessário quando a requisição
+          //for bem-sucedida
+          this.jogos = jogos;
 
-  ngOnInit(): void {}
+        }
+      });
+  }
 }
