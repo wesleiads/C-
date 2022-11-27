30e8bb7dfb8e71fe94fe032448ae8b134712e60d
Weslei Barbosa <101604299+wesleiads@users.noreply.github.com>
2022-11-27 10:40:34 -0300

    Update jogo.model.ts

--- FRONT_API/src/app/models/jogo.model.ts
+++ FRONT_API/src/app/models/jogo.model.ts
@@ -3,9 +3,7 @@ import { Selecao } from "./selecao.model";
 export interface Jogo {
   id?: number;
   SelecaoA?: Selecao;
-  selecaoAId?: number,
   SelecaoB?: Selecao;
-  selecaoBId?: number,
   criadoEm?: string;
   placar?: Number;
   placar1?: Number;
